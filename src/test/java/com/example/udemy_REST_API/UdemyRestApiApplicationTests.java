1647964358d4c48267bbc06d23b050b8c0262dbc
AlessandroSalomonMac <>
2024-05-17 21:38:35 +0200

    initial

---
+++ src/test/java/com/example/udemy_REST_API/UdemyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udemy_REST_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
